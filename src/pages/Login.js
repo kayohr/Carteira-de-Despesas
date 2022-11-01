47cf61bb21fd8258f727c8e7dd66f47e805a4fbd
kayo <kayohramos@gmail.com>
2022-10-31 21:25:25 -0300

    parte 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,83 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+import { addUser } from '../redux/actions';
+
+const number = 6;
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    buttonDisable: true,
+  };
+
+  handleChange = (event) => {
+    const { name, type, checked } = event.target;
+    const value = type === 'checkbox' ? checked : event.target.value;
+    this.setState({
+      [name]: value,
+    }, this.validateButton);
+  };
+
+  validateButton = () => {
+    const { email, password } = this.state;
+    const senha = password.length >= number;
+    const acessEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+?$/i;
+    const validateEmail = acessEmail.test(email);
+    const enable = senha && validateEmail;
+    this.setState({
+      buttonDisable: !enable,
+    });
+  };
+
+  submitLogin = (event) => {
+    event.preventDefault();
+    const { dispatch, history } = this.props;
+    const { email } = this.state;
+    dispatch(addUser(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { buttonDisable } = this.state;
+    return (
+      <form
+        onSubmit={ this.submitLogin }
+      >
+        Login
+        <input
+          placeholder="Digite o seu email aqui"
+          data-testid="email-input"
+          name="email"
+          type="text"
+          onChange={ this.handleChange }
+        />
+
+        <input
+          data-testid="password-input"
+          name="password"
+          type="password"
+          placeholder="Digite o seu password aqui"
+          onChange={ this.handleChange }
+        />
+
+        <button
+          type="submit"
+          disabled={ buttonDisable }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,12 @@
-// Coloque aqui suas actions
+export const ADD_USER = 'ADD_USER';
+export const ADD_WALLET = 'ADD_WALLET';
+
+export const addUser = (email) => ({
+  type: ADD_USER,
+  email,
+});
+
+export const addWallet = (wallet) => ({
+  type: ADD_WALLET,
+  wallet,
+});
